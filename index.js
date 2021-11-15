69c82d5ca60fb83b584724493f3de2009286306f
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-11-15 11:09:42 -0300

    Adiciona validação de formulário

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+class SchoolData {
+    static fromObject(obj) {
+        return new SchoolData(
+            obj["school-name"],
+            obj["inep-code"],
+            obj["current-process"],
+            obj["offered-modalities"],
+            obj["resolution"]
+        )
+    }
+
+    constructor(schoolName, inepCode, currentProcess, offeredModalities, resolution) {
+        this.schoolName = schoolName;
+        this.inepCode = inepCode;
+        this.offeredModalities = offeredModalities;
+        this.currentProcess = currentProcess;
+        this.resolution = resolution;
+    }
+}
+
 class InputModel {
-    constructor(label, type, required) {
+    constructor(id, label, type, required) {
+        this.id = id;
         this.label = label;
         this.type = type;
         this.required = required;
@@ -7,14 +28,14 @@ class InputModel {
 }
 
 function main() {
-    const container = document.getElementById("form-container");
+    const form = document.getElementById("form-container");
 
     const models = [
-        new InputModel("Nome da escola", "text", true),
-        new InputModel("Código INEP", "text", true),
-        new InputModel("Etapa(s)/Modalidade(s) de ensino ofertada(s)", "text", true),
-        new InputModel("Processo atual", "text", false),
-        new InputModel("Resolução", "text", false),
+        new InputModel("school-name", "Nome da escola", "text", true),
+        new InputModel("inep-code", "Código INEP", "text", true),
+        new InputModel("offered-modalities", "Etapa(s)/Modalidade(s) de ensino ofertada(s)", "text", true),
+        new InputModel("current-process", "Processo atual", "text", false),
+        new InputModel("resolution", "Resolução", "text", false),
     ];
 
     for (let i = 0; i < models.length; i++) {
@@ -24,14 +45,38 @@ function main() {
         p.innerText = model.label
         p.style.gridRow = `${i + 2}`
         p.style.gridColumn = "2"
-        container.appendChild(p)
+        form.appendChild(p)
 
         const input = document.createElement("input")
+        input.id = `form.${model.id}`
         input.type = model.type
         input.required = model.required;
         input.style.gridRow = `${i + 2}`
         input.style.gridColumn = "3"
-        container.appendChild(input)
+        form.appendChild(input)
+    }
+
+    const button = document.getElementById("save-button")
+    button.onclick = function (_) {
+        const matrix = models.map(model => {
+            const id = model.id;
+            const element = document.getElementById(`form.${id}`);
+            return [model, element.value];
+        });
+
+        const missingLabels = matrix
+            .filter(([model, text]) => !text && model.required)
+            .map(([model, _]) => model.label);
+
+        if (missingLabels.length) {
+            alert("Os campos\n\n" + missingLabels
+                .map(label => "- " + label)
+                .join("\n") + "\n\n são obrigatórios.")
+            return;
+        }
+
+        const obj = new Map(matrix.map(([model, text]) => [model.id, text]));
+        const data = SchoolData.fromObject(Object.fromEntries(obj));
     }
 }
 
