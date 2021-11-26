86c5a9fffd516ac05a7c6b2872c1b854ab935073
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-11-26 20:45:05 -0300

    Adiciona suporte à rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from "./home/HomeScreen";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SchoolsTable from "./schools-table/SchoolsTable";
 
 class App extends React.Component {
     render() {
-        return (<HomeScreen/>);
+        return <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomeScreen/>}/>
+                <Route path="/consultaEscola" element={<SchoolsTable/>}/>
+            </Routes>
+        </BrowserRouter>;
     }
 }
 
--- src/home/HomeScreenTile.tsx
+++ src/home/HomeScreenTile.tsx
@@ -4,13 +4,18 @@ import './HomeScreenTile.css';
 export interface HomeScreenTileProps {
     name: string;
     icon: string;
+    route?: string;
 }
 
 export class HomeScreenTile extends React.Component<HomeScreenTileProps, {}> {
     render() {
         return <div className="HomeScreenTile">
             <div className="HomeScreenTile-bg">
-                <div className="HomeScreenTile-fg">
+                <div className="HomeScreenTile-fg" onClick={() => {
+                    const route = this.props.route;
+                    if (route == null) return;
+                    window.location.href = route;
+                }}>
                     <p className="HomeScreenTile-label">{this.props.name}</p>
                     <img style={{height: "100px", width: undefined}} src={this.props.icon}/>
                 </div>
