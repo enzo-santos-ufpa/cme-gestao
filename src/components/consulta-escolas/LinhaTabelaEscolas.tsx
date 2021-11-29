a93060a1ab656e99b2a658686ae8f783a120360e
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-11-29 15:22:23 -0300

    Adiciona barra de status para escolas

--- src/components/consulta-escolas/LinhaTabelaEscolas.tsx
+++ src/components/consulta-escolas/LinhaTabelaEscolas.tsx
@@ -5,7 +5,7 @@ import {Processo} from "../../models/tipos";
 
 type LinhaChaveValorProps = { chave: string, valor: string }
 
-type LinhaTabelaEscolasProps = { escola: Escola }
+type LinhaTabelaEscolasProps = { escola: Escola, mostraStatus: boolean }
 
 class LinhaChaveValor extends React.Component<LinhaChaveValorProps, {}> {
     render() {
@@ -17,7 +17,8 @@ class LinhaChaveValor extends React.Component<LinhaChaveValorProps, {}> {
 }
 
 class LinhaTabelaEscolas extends React.Component<LinhaTabelaEscolasProps, {}> {
-    private static renderProcessDiv(processo: Processo): JSX.Element {
+    private static renderizaDescricaoProcesso(processo?: Processo): JSX.Element | null {
+        if (processo == null) return null;
         return <div style={{display: "flex"}}>
             <div style={{display: "flex", flexDirection: "column"}}>
                 <LinhaChaveValor chave="Processo atual" valor={processo.nome}/>
@@ -40,12 +41,29 @@ class LinhaTabelaEscolas extends React.Component<LinhaTabelaEscolasProps, {}> {
         </div>
     }
 
+    private renderizaStatus(processo?: Processo): JSX.Element | null {
+        if (processo == null) return null;
+        if (!this.props.mostraStatus) return null;
+        const cor = LinhaTabelaEscolas.mapeiaCor(processo.diasRestantes);
+        return <div className="LinhaTabelaEscolas-status" style={{backgroundColor: cor}}/>
+    }
+
+    private static mapeiaCor(diasRestantes: number): string {
+        if (diasRestantes > 120) return "#7ED957";
+        if (diasRestantes > 100) return "#FFDE59";
+        if (diasRestantes > 90) return "#FFA500";
+        return "#FF1616";
+    }
+
     render() {
         const processoAtual = this.props.escola.processoAtual;
         return (
-            <div>
-                <p className="LinhaTabelaEscolas-titulo">{this.props.escola.nome}</p>
-                {processoAtual == null ? null : LinhaTabelaEscolas.renderProcessDiv(processoAtual)}
+            <div className="LinhaTabelaEscolas">
+                {this.renderizaStatus(processoAtual)}
+                <div>
+                    <p className="LinhaTabelaEscolas-titulo">{this.props.escola.nome}</p>
+                    {LinhaTabelaEscolas.renderizaDescricaoProcesso(processoAtual)}
+                </div>
             </div>
         );
     }
