0a88bd8004e05dc68e6048203097253630db239e
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-12-02 11:11:25 -0300

    Remove warning de variável inutilizada

--- src/components/consulta-escolas/TelaConsultaEscolas.tsx
+++ src/components/consulta-escolas/TelaConsultaEscolas.tsx
@@ -2,39 +2,8 @@ import React from "react";
 import './TelaConsultaEscolas.css';
 import LinhaTabelaEscolas from "./LinhaTabelaEscolas";
 import Escola from "../../models/Escola";
-import {DistritoAdmnistrativo, Processo, SetorEscola} from "../../models/tipos";
 import {escolas} from "../../lib/api";
 import PlanoFundo, {bg} from "../common/PlanoFundo";
-
-const escolaPadrao: Escola = {
-    bairro: "CARANANDUBA - MOSQUEIRO",
-    cep: "66923-060",
-    cidade: "Belém",
-    cnpj: "",
-    cnpjConselho: "",
-    codigoInep: "15043274",
-    contatoDiretor: {telefone: "", email: "", whatsapp: ""},
-    contatoSecretario: {telefone: "", email: "", whatsapp: ""},
-    convenioSemec: undefined,
-    dataCriacao: new Date(2019, 1, 1),
-    distrito: DistritoAdmnistrativo.DAMOS,
-    email: "",
-    endereco: "RUA LALOR MOTA, 551",
-    filiais: [],
-    modalidadeEnsino: {nome: "", etapa: {nome: ""}},
-    nome: "ABEL MARTINS",
-    processoAtual: new Processo(
-        "055/17-CME",
-        "53/19-CME",
-        new Date(2017, 12, 12),
-        5,
-    ),
-    sigla: "",
-    telefone: "",
-    tipo: {setor: SetorEscola.publico, nome: "EMEF"},
-    uf: "PA",
-    vigenciaConselho: ""
-}
 
 function divideElementos(elementos: JSX.Element[], divisor: JSX.Element): JSX.Element[] {
     const resultado: JSX.Element[] = [divisor];
