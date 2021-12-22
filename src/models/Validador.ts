f5371e3c218872cb9f4acdd62ba33b28e9bec4d1
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-12-22 20:50:25 -0300

    Generaliza interface de validação

--- src/models/Validador.ts
+++ src/models/Validador.ts
@@ -1,15 +1,19 @@
 import {parseDate} from "../lib/utils";
 
-type MetodoValidacao = (texto: string) => string | undefined;
+type MetodoValidacao<T> = (valor: T) => string | undefined;
 
-export default class Validador {
-    private readonly metodo: MetodoValidacao;
+export default class Validador<T> {
+    static texto(): Validador<string> {
+        return new Validador<string>();
+    }
 
-    constructor(metodo?: MetodoValidacao) {
+    private readonly metodo: MetodoValidacao<T>;
+
+    constructor(metodo?: MetodoValidacao<T>) {
         this.metodo = metodo ?? ((_) => undefined);
     }
 
-    use(metodo: MetodoValidacao): Validador {
+    use(metodo: MetodoValidacao<T>): Validador<T> {
         return new Validador((texto) => {
             const erro = this.metodo(texto);
             if (erro != null) return erro;
@@ -17,25 +21,29 @@ export default class Validador {
         });
     }
 
-    validate(texto: string): string | undefined {
-        return this.metodo(texto);
+    validate(valor: T): string | undefined {
+        return this.metodo(valor);
     }
 }
 
 export namespace Validadores {
-    export function required(erro?: string): MetodoValidacao {
+    export function min1(erro?: string): MetodoValidacao<Array<any>> {
+        return (valor) => !valor.length ? (erro ?? "Selecione pelo menos um valor.") : undefined;
+    }
+
+    export function required(erro?: string): MetodoValidacao<string> {
         return (texto) => !texto.length ? (erro ?? "Insira este campo.") : undefined;
     }
 
-    export function cnpj(erro?: string): MetodoValidacao {
+    export function cnpj(erro?: string): MetodoValidacao<string> {
         return (texto) => !texto.match(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/) ? (erro ?? "O CNPJ deve estar no formato XX.XXX.XXX/XXXX-XX.") : undefined;
     }
 
-    export function date(erro?: string): MetodoValidacao {
+    export function date(erro?: string): MetodoValidacao<string> {
         return (texto) => parseDate(texto) == null ? (erro ?? "A data deve estar no formato DD/MM/YYYY.") : undefined;
     }
 
-    export function cep(erro?: string): MetodoValidacao {
+    export function cep(erro?: string): MetodoValidacao<string> {
         return (texto) => !texto.match(/^\d{3}\.\d{2}-\d{3}$/) ? (erro ?? "O CEP deve estar no formato XXX.XX-XXX.") : undefined;
     }
 }
