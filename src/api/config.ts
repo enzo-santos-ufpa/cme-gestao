e2f8a8bae08cf3f8c439446b3d68d45248bacdd9
Enzo Santos <enzogabrielrocha29@gmail.com>
2021-12-22 20:43:34 -0300

    Melhora modelo entidade-relacionamento

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,91 +1,225 @@
 import pg from "pg";
+import yesql from "yesql";
 
-async function initialize(pool: pg.Pool) {
-    await pool.query(`CREATE TABLE IF NOT EXISTS 
-        Escola (
-            Id SERIAL NOT NULL,
-            Nome VARCHAR(255) NOT NULL,
-            Sigla VARCHAR(255) NOT NULL,
-            DataCriacao DATE NOT NULL,
-            CodigoINEP VARCHAR(8) NOT NULL,
-            CNPJ VARCHAR(18) NOT NULL,
-            CNPJConselho VARCHAR(18) NOT NULL,
-            VigenciaConselho VARCHAR(255) NOT NULL,
-            NomeEntidadeMantenedora VARCHAR(255) NOT NULL,
-            Email VARCHAR(255) NOT NULL,
-            Telefone VARCHAR(15) NOT NULL,
-            TipoSetor VARCHAR(255) NOT NULL,
-            TipoSigla VARCHAR(255) NOT NULL,
-            PRIMARY KEY (Id)
-        );
-    `);
-    await pool.query(`CREATE TABLE IF NOT EXISTS 
-        EnderecoEscola (
-            Id SERIAL NOT NULL,
-            IdEscola INTEGER NOT NULL,
-            Distrito VARCHAR(30) NOT NULL,
-            Endereco VARCHAR(255) NOT NULL,
-            Cidade VARCHAR(255) NOT NULL,
-            UF VARCHAR(2) NOT NULL,
-            Bairro VARCHAR (255) NOT NULL,
-            CEP VARCHAR(10) NOT NULL,
-            PRIMARY KEY (Id),
-            FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
-        );
-    `);
-    await pool.query(`CREATE TABLE IF NOT EXISTS 
-        ServidorEscola (
-            Id SERIAL NOT NULL,
-            IdEscola INTEGER NOT NULL,
-            Tipo VARCHAR(255) NOT NULL,
-            Nome VARCHAR(255) NOT NULL,
-            Email VARCHAR(255) NOT NULL,
-            Telefone VARCHAR(15) NOT NULL,
-            PRIMARY KEY (Id),
-            FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
-        );
-    `)
-    await pool.query(`CREATE TABLE IF NOT EXISTS 
-        ProcessoEscola(
-            Id SERIAL NOT NULL,
-            IdEscola INTEGER NOT NULL,
-            Nome VARCHAR(255) NOT NULL,
-            Resolucao VARCHAR(255) NOT NULL,
-            TempoVigencia INTEGER NOT NULL,
-            DataInicioVigencia DATE NOT NULL,
-            PRIMARY KEY (Id),
-            FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
-        );
-    `);
-    await pool.query(`CREATE TABLE IF NOT EXISTS 
-        TriagemEscola (
-            Id SERIAL NOT NULL,
-            IdEscola INTEGER NOT NULL,
-            DataInsercao DATE NOT NULL,
-            PRIMARY KEY (Id),
-            FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
-        );
-    `);
-    await pool.query(`CREATE TABLE IF NOT EXISTS
-        ConvenioSEMEC (
-            NumeroConvenio INTEGER NOT NULL,
-            IdEscola INTEGER NOT NULL,
-            Objeto VARCHAR(255) NOT NULL,
-            Vigencia VARCHAR(255) NOT NULL,
-            PRIMARY KEY (NumeroConvenio),
-            FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
-        );
-    `);
+class Database {
+    private readonly _pool: pg.Pool;
+    private readonly _promise: Promise<void>;
+
+    private static async initialize(pool: pg.Pool): Promise<void> {
+        // language=SQL format=false
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS SetorEscola (
+                Id INTEGER NOT NULL,
+                Nome VARCHAR(30) UNIQUE NOT NULL,
+                PRIMARY KEY (Id)
+            );
+            INSERT INTO SetorEscola (Id, Nome) VALUES
+                (1, 'Pública'),
+                (2, 'Privada');
+
+            CREATE TABLE IF NOT EXISTS SiglaEscola (
+                Id INTEGER NOT NULL,
+                IdSetor INTEGER NOT NULL,
+                Nome VARCHAR(30) UNIQUE NOT NULL,
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdSetor) REFERENCES SetorEscola (Id)
+            );
+            INSERT INTO SiglaEscola (Id, IdSetor, Nome) VALUES
+                (1, 1, 'EMEIF'),
+                (2, 1, 'EMEF'),
+                (3, 1, 'EMEI'),
+                (4, 1, 'UEI'),
+                (5, 2, 'OSC'),
+                (6, 2, 'Comunitária'),
+                (7, 2, 'Confessional'),
+                (8, 2, 'Privada');
+
+            CREATE TABLE IF NOT EXISTS EtapaEnsino (
+                Id INTEGER NOT NULL,
+                Nome VARCHAR(30) UNIQUE NOT NULL,
+                PRIMARY KEY (Id)
+            );
+            INSERT INTO EtapaEnsino (Id, Nome) VALUES
+                (1, 'Educação infantil'),
+                (2, 'Educação fundamental'),
+                (3, 'Educação fundamental (EJA)');
+
+            CREATE TABLE IF NOT EXISTS ModalidadeEnsino (
+                Id INTEGER NOT NULL,
+                IdEtapa INTEGER NOT NULL,
+                Nome VARCHAR(30) NOT NULL,
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdEtapa) REFERENCES EtapaEnsino (Id)
+            );
+            INSERT INTO ModalidadeEnsino (Id, IdEtapa, Nome) VALUES
+                (1, 1, 'Creche'),
+                (2, 1, 'Pré-escola'),
+                (3, 2, 'CF I (1º, 2º e 3º ano)'),
+                (4, 2, 'CF II (4º e 5º ano)'),
+                (5, 2, 'CF III (6º e 7º ano)'),
+                (6, 2, 'CF IV (8º e 9º ano)'),
+                (7, 3, 'CF I (1º, 2º e 3º ano)'),
+                (8, 3, 'CF II (4º e 5º ano)'),
+                (9, 3, 'CF III (6º e 7º ano)'),
+                (10, 3, 'CF IV (8º e 9º ano)');
+                  
+            CREATE TABLE IF NOT EXISTS DocumentoCadastro (
+                Id INTEGER NOT NULL,
+                Nome VARCHAR(255) NOT NULL,
+                PRIMARY KEY (Id)
+            );
+            INSERT INTO DocumentoCadastro (Id, Nome) VALUES
+                (1, 'Requerimento'),
+                (2, 'Regimento escolar'),
+                (3, 'Projeto pedagógico'),
+                (4, 'Quadro demonstrativo'),
+                (5, 'Cronograma de implantação'),
+                (6, 'Detalhamento de implantação e desenvolvimento'),
+                (7, 'Declaração de equipamentos'),
+                (8, 'Alvará de funcionamento'),
+                (9, 'Laudo da vigilância sanitária'),
+                (10, 'Laudo do corpo de bombeiros'),
+                (11, 'Projeto com promoção e acessibilidade'),
+                (12, 'Relatório detalhado das condições de oferta dos cursos'),
+                (13, 'Comprovante de entrega de censo'),
+                (14, 'Escolas anexas no processo da escola matriz'),
+                (15, 'Relação dos alunos'),
+                (16, 'Plano de cursos'),
+                (17, 'Termo de convênio para prática profissional'),
+                (18, 'Requerimento dirigido à presidência do CME'),
+                (19, 'Comprovante dos atos constitutivos'),
+                (20, 'Comprovante de inscrição'),
+                (21, 'Comprovante de inscrição no cadastro de contribuição municipal'),
+                (22, 'Certidões de regularidades fiscais'),
+                (23, 'Certidões de regularidades FGTS'),
+                (24, 'Demonstração de patrimônio'),
+                (25, 'Biblioteca'),
+                (26, 'Acessibilidade'),
+                (27, 'Laboratório de informática'),
+                (28, 'Sala de recursos multifuncionais'),
+                (29, 'Área esportiva'),
+                (30, 'Brinquedoteca');
+
+            CREATE TABLE IF NOT EXISTS Escola (
+                Id SERIAL NOT NULL,
+                Nome VARCHAR(255) NOT NULL,
+                DataCriacao DATE NOT NULL,
+                CodigoINEP CHAR(8) NOT NULL, -- XXXXXXXX
+                CNPJ CHAR(18) NOT NULL, -- XX.XXX.XXX/XXXX-XX
+                CNPJConselho CHAR(18) NOT NULL, -- XX.XXX.XXX/XXXX-XX
+                VigenciaConselho INTEGER NOT NULL,
+                NomeEntidadeMantenedora VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL,
+                Telefone CHAR(15) NOT NULL, -- (XX) XXXXX-XXXX 
+                IdSigla INTEGER NOT NULL,
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdSigla) REFERENCES SiglaEscola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS EnderecoEscola (
+                IdEscola INTEGER NOT NULL,
+                Distrito CHAR(5) NOT NULL,
+                Endereco VARCHAR(255) NOT NULL,
+                Cidade VARCHAR(255) NOT NULL,
+                UF CHAR(2) NOT NULL,
+                Bairro VARCHAR (255) NOT NULL,
+                CEP CHAR(10) NOT NULL, -- XXX.XX-XXX
+                PRIMARY KEY (IdEscola),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS ServidorEscola (
+                Id SERIAL NOT NULL,
+                IdEscola INTEGER NOT NULL,
+                Tipo VARCHAR(255) NOT NULL,
+                Nome VARCHAR(255) NOT NULL,
+                Email VARCHAR(255) NOT NULL,
+                Telefone CHAR(15) NOT NULL, -- (XX) XXXXX-XXXX
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS ProcessoEscola (
+                Id SERIAL NOT NULL,
+                IdEscola INTEGER NOT NULL,
+                Nome VARCHAR(255) NOT NULL,
+                Resolucao VARCHAR(255) NOT NULL,
+                DataInicioVigencia DATE NOT NULL,
+                TempoVigencia INTEGER NOT NULL, -- Em anos
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS TriagemEscola (
+                IdEscola INTEGER NOT NULL,
+                DataInsercao DATE NOT NULL,
+                PRIMARY KEY (IdEscola),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS ConvenioSEMEC (
+                Id INTEGER NOT NULL,
+                IdEscola INTEGER NOT NULL,
+                Objeto VARCHAR(255) NOT NULL,
+                Vigencia DATE NOT NULL,
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS ModalidadeEscola (
+                IdEscola INTEGER NOT NULL,
+                IdModalidade INTEGER NOT NULL,
+                PRIMARY KEY (IdEscola),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id),
+                FOREIGN KEY (IdModalidade) REFERENCES ModalidadeEnsino (Id)
+            );
+            CREATE TABLE IF NOT EXISTS Filial (
+                Id SERIAL NOT NULL,
+                IdEscola INTEGER NOT NULL,
+                Nome VARCHAR(255) NOT NULL,
+                DataCriacao DATE NOT NULL,
+                CodigoINEP CHAR(8) NOT NULL, -- XXXXXXXX
+                Email VARCHAR(255) NOT NULL,
+                Telefone CHAR(15) NOT NULL, -- (XX) XXXXX-XXXX 
+                IdSigla INTEGER NOT NULL,
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id),
+                FOREIGN KEY (IdSigla) REFERENCES SiglaEscola (Id)
+            );
+            CREATE TABLE IF NOT EXISTS DocumentoEnviado (
+                Id SERIAL NOT NULL,
+                IdDocumento INTEGER NOT NULL,
+                IdEscola INTEGER NOT NULL,
+                Localizacao VARCHAR(255),
+                PRIMARY KEY (Id),
+                FOREIGN KEY (IdDocumento) REFERENCES DocumentoCadastro (Id),
+                FOREIGN KEY (IdEscola) REFERENCES Escola (Id)
+            );
+        `);
+    }
+
+    private async pool(): Promise<pg.Pool> {
+        await this._promise;
+        return this._pool;
+    }
+
+    constructor() {
+        this._pool = new pg.Pool({connectionString: process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/postgres"});
+        this._promise = Database.initialize(this._pool);
+    }
+
+    async named<R extends pg.QueryResultRow = any>(
+        queryTextOrConfig: string,
+        values: Record<string, any>,
+    ): Promise<pg.QueryResult<R>> {
+        const pool = await this.pool();
+        return pool.query(yesql.pg(queryTextOrConfig)(values));
+    }
+
+    async positional<R extends pg.QueryResultRow = any, I extends any[] = any[]>(
+        queryTextOrConfig: string | pg.QueryConfig<I>,
+        values?: I,
+    ): Promise<pg.QueryResult<R>> {
+        const pool = await this.pool();
+        return pool.query(queryTextOrConfig, values);
+    }
+
+
 }
 
-
-const pool = new pg.Pool({connectionString: process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/postgres"});
-initialize(pool).then((_) => console.log("backend: Tabelas inicializadas"));
-
-pool.on('connect', () => {
-    console.log('backend: Conexão feita ao banco');
-});
-
-export const db = {
-    pool: pool,
-};
+const db = new Database();
+export default db;
--- src/api/controllers.ts
+++ src/api/controllers.ts
@@ -1,4 +1,4 @@
-import {db} from "./config";
+import db from "./config";
 import express from "express";
 import {
     encoding,
@@ -34,8 +34,10 @@ export namespace escolas {
             };
         }
 
-        const consulta = await db.pool.query(
-            `SELECT * FROM ServidorEscola WHERE IdEscola = $1`,
+        const consulta = await db.positional(
+            `SELECT *
+             FROM ServidorEscola
+             WHERE IdEscola = $1`,
             [id],
         );
         const mapeamento = new Map<string, any>();
@@ -104,66 +106,84 @@ export namespace escolas {
     }
 
     export async function criar(req: express.Request, res: express.Response) {
-        async function insereServidor(id: number, tipo: "Diretor" | "Secretario" | "Coordenador", servidor: Servidor) {
-            await db.pool.query(`INSERT INTO
-                ServidorEscola (IdEscola, Tipo, Nome, Email, Telefone)
-                VALUES         (      $1,   $2,   $3,    $4,       $5)`,
-                [id, tipo, servidor.nome, servidor.email, servidor.telefone],
-            );
-        }
-
         try {
-            const data = req.body as FlatEncoded<EscolaBase>;
+            let consulta;
+            const data = encoding.escolaBase().decode(req.body as FlatEncoded<EscolaBase>);
 
-            const {
-                nome,
-                sigla,
-                dataCriacao,
-                codigoInep,
-                cnpj,
-                cnpjConselho,
-                vigenciaConselho,
-                nomeEntidadeMantenedora,
-                email,
-                telefone,
-                tipo,
-                ...a0
-            } = encoding.escolaBase().decode(data);
-
-            const consulta = await db.pool.query(`INSERT INTO 
-                Escola (Nome, Sigla, DataCriacao, CodigoINEP, CNPJ, CNPJConselho, VigenciaConselho, NomeEntidadeMantenedora, Email, Telefone, TipoSetor, TipoSigla)
-                VALUES (  $1,    $2,          $3,         $4,   $5,           $6,               $7,                      $8,    $9,      $10,       $11,       $12)
-                RETURNING Id`,
-                [nome, sigla, dataCriacao, codigoInep, cnpj, cnpjConselho, vigenciaConselho, nomeEntidadeMantenedora, email, telefone, tipo.setor, tipo.sigla]
-            );
-
-            const {id} = consulta.rows.pop();
-
-            await db.pool.query(`INSERT INTO 
-                TriagemEscola (IdEscola, DataInsercao)
-                VALUES        (      $1,           $2)`,
-                [id, new Date()],
-            );
-
-            const {distrito, endereco, cidade, uf, bairro, cep, ...a1} = a0;
-            await db.pool.query(`INSERT INTO
-                EnderecoEscola (IdEscola, Distrito, Endereco, Cidade, UF, Bairro, CEP)
-                VALUES         (      $1,       $2,       $3,     $4, $5,     $6,  $7)`,
-                [id, distrito, endereco, cidade, uf, bairro, cep],
-            );
-
-            const {diretor, coordenador, secretario, ...a2} = a1;
-            await insereServidor(id, "Diretor", diretor);
-            await insereServidor(id, "Coordenador", coordenador);
-            await insereServidor(id, "Secretario", secretario);
-
-            const {convenioSemec, ...a3} = a2;
-            if (convenioSemec != null) {
-                await db.pool.query(`INSERT INTO
-                    ConvenioSEMEC (NumeroConvenio, IdEscola, Objeto, Vigencia)
-                    VALUES        (            $1,       $2,     $3,       $4)`,
-                    [convenioSemec.numConvenio, id, convenioSemec.objeto, convenioSemec.vigencia]);
+            // language=SQL format=false
+            consulta = await db.positional(`SELECT Id FROM SiglaEscola WHERE Nome = $1`, [data.sigla]);
+            if (!consulta.rows.length) {
+                return res.status(401).send({error: `Sigla inválida: ${data.sigla}`});
             }
+            const idSigla = consulta.rows.pop()["id"];
+
+            // language=SQL format=false
+            consulta = await db.named(`INSERT INTO
+                Escola (Nome, DataCriacao, CodigoINEP, CNPJ, CNPJConselho, VigenciaConselho, NomeEntidadeMantenedora, Email, Telefone, IdSigla)
+                VALUES(:nome, :dataCriacao, :codigoInep, :cnpj, :cnpjConselho, :vigenciaConselho, :nomeEntidadeMantenedora, :email, :telefone, :idSigla)`,
+                {...data, idSigla});
+
+            const escola: ModeloBD<EscolaBase> = {...data, id: consulta.rows.pop()};
+
+            // language=SQL format=false
+            await db.named(`INSERT INTO
+                EnderecoEscola (IdEscola, Distrito, Endereco, Cidade, UF, Bairro, CEP)
+                VALUES (:id, :distrito, :endereco, :cidade, :uf, :bairro, :cep)`,
+                {...escola});
+
+            for (const modalidade of escola.modalidadesEnsino) {
+                // language=SQL format=false
+                consulta = await db.positional(`SELECT
+                    ModalidadeEnsino.Id
+                    FROM ModalidadeEnsino
+                    JOIN EtapaEnsino ON EtapaEnsino.Id = ModalidadeEnsino.IdEtapa
+                    WHERE EtapaEnsino.Nome = $1 AND ModalidadeEnsino.Nome = $2`,
+                    [modalidade.etapa, modalidade.nome]);
+
+                if (!consulta.rows.length) {
+                    return res.status(401).send({error: `Modalidade inválida: ${modalidade}`});
+                }
+                const idModalidade = consulta.rows.pop()["id"];
+
+                // language=SQL format=false
+                await db.positional(`INSERT INTO
+                    ModalidadeEscola (IdEscola, IdModalidade)
+                    VALUES ($1, $2)`,
+                    [escola.id, idModalidade])
+            }
+
+            const convenio = escola.convenioSemec;
+            if (convenio != null) {
+                // language=SQL format=false
+                await db.positional(`INSERT INTO
+                    ConvenioSEMEC (Id, IdEscola, Objeto, Vigencia)
+                    VALUES ($1, $2, $3, $4)`,
+                    [convenio.numConvenio, escola.id, convenio.objeto, convenio.vigencia])
+            }
+
+            const servidores: Record<string, Servidor> = {
+                diretor: escola.diretor,
+                secretario: escola.secretario,
+                coordenador: escola.coordenador
+            };
+            for (const [tipo, servidor] of Object.entries(servidores)) {
+                if (servidor == null) continue;
+
+                // language=SQL format=false
+                await db.positional(`INSERT INTO
+                    ServidorEscola (IdEscola, Tipo, Nome, Email, Telefone)
+                    VALUES ($1, $2, $3, $4, $5)`,
+                    [escola.id, tipo, servidor.nome, servidor.email, servidor.telefone]);
+            }
+
+            // TODO add DocumentoEnviado
+            // TODO add Filial
+
+            // language=SQL format=false
+            await db.positional(`INSERT INTO
+                TriagemEscola (IdEscola, DataInsercao)
+                VALUES ($1, $2)`,
+                [escola.id, new Date()]);
 
         } catch (e) {
             return res.status(401).send({error: e + ""});
@@ -172,10 +192,41 @@ export namespace escolas {
     }
 
     export async function consultar(req: express.Request, res: express.Response) {
-        const consulta = await db.pool.query(`SELECT * FROM Escola 
+        let consulta;
+
+        // language=SQL format=false
+        consulta = await db.positional(`SELECT 
+                Escola.Id,
+                Escola.Nome,
+                Escola.DataCriacao,
+                Escola.CodigoINEP,
+                Escola.CNPJ,
+                Escola.CNPJConselho,
+                Escola.VigenciaConselho,
+                Escola.NomeEntidadeMantenedora,
+                Escola.Email,
+                Escola.Telefone,
+                EnderecoEscola.Distrito,
+                EnderecoEscola.Endereco,
+                EnderecoEscola.Cidade,
+                EnderecoEscola.UF,
+                EnderecoEscola.Bairro,
+                ConvenioSemec.Id as IdConvenioSEMEC,
+                ConvenioSemec.Objeto,
+                ConvenioSemec.Vigencia,
+                ProcessoEscola.Id,
+                ProcessoEscola.Nome,
+                ProcessoEscola.Resolucao,
+                ProcessoEscola.DataInicioVigencia,
+                ProcessoEscola.TempoVigencia,
+                TriagemEscola.DataInsercao as DataInsercaoTriagem
+            FROM Escola
             JOIN EnderecoEscola ON Escola.Id = EnderecoEscola.IdEscola
             LEFT JOIN ConvenioSEMEC ON Escola.Id = ConvenioSEMEC.IdEscola
-            WHERE Escola.Id = $1`, [req.query.id]);
+            LEFT JOIN ProcessoEscola ON Escola.Id = ProcessoEscola.IdEscola
+            LEFT JOIN TriagemEscola ON Escola.Id = TriagemEscola.IdEscola 
+            WHERE Escola.Id = $1`,
+            [req.query.id]);
 
         const rows = consulta.rows;
         switch (rows.length) {
@@ -184,6 +235,21 @@ export namespace escolas {
             }
             case 1: {
                 const row = rows.pop();
+                const {id} = row;
+                // language=SQL format=false
+                consulta = await db.positional(`SELECT
+                        (
+                            SELECT
+                                EtapaEnsino.Nome as NomeEtapa,
+                                ModalidadeEnsino.Nome as NomeModalidade
+                            FROM ModalidadeEnsino
+                            JOIN EtapaEnsino ON EtapaEnsino.Id = ModalidadeEnsino.IdEtapa
+                            WHERE ModalidadeEnsino.Id = ModalidadeEscola.IdModalidade
+                        )
+                    FROM ModalidadeEscola
+                    WHERE ModalidadeEscola.IdEscola = $1`,
+                    [id]);
+
                 const encoder = encoding.modeloDB(encoding.escolaBase());
                 const escola = await parseEscolaPendente(row);
                 return res.status(200).send(encoder.encode(escola));
@@ -199,10 +265,11 @@ export namespace escolas {
         const data: ModeloBD<EscolaPendente>[] = [];
 
         try {
-            const consulta = await db.pool.query(`SELECT * FROM Escola
-                JOIN EnderecoEscola ON Escola.Id = EnderecoEscola.IdEscola
-                LEFT JOIN ConvenioSEMEC ON Escola.Id = ConvenioSEMEC.IdEscola
-                RIGHT JOIN TriagemEscola ON Escola.Id = TriagemEscola.IdEscola`);
+            const consulta = await db.positional(`SELECT *
+                                             FROM Escola
+                                                      JOIN EnderecoEscola ON Escola.Id = EnderecoEscola.IdEscola
+                                                      LEFT JOIN ConvenioSEMEC ON Escola.Id = ConvenioSEMEC.IdEscola
+                                                      RIGHT JOIN TriagemEscola ON Escola.Id = TriagemEscola.IdEscola`);
 
             for (const row of consulta.rows) {
                 data.push(await parseEscolaPendente(row));
@@ -218,16 +285,15 @@ export namespace escolas {
         const encoder = encoding.lista(encoding.modeloDB(encoding.escolaAutorizada()));
         const data: ModeloBD<EscolaAutorizada>[] = [];
         try {
-            const consulta = await db.pool.query(`
-                SELECT 
-                    *,
-                    Escola.Id as EscolaId
+            const consulta = await db.positional(`
+                SELECT *,
+                       Escola.Id AS EscolaId
                 FROM Escola
-                JOIN EnderecoEscola ON Escola.Id = EnderecoEscola.IdEscola
-                LEFT JOIN ConvenioSEMEC ON Escola.Id = ConvenioSEMEC.IdEscola
-                LEFT JOIN TriagemEscola ON Escola.Id = TriagemEscola.IdEscola
-                WHERE TriagemEscola.Id IS NULL;
-                `);
+                         JOIN EnderecoEscola ON Escola.Id = EnderecoEscola.IdEscola
+                         LEFT JOIN ConvenioSEMEC ON Escola.Id = ConvenioSEMEC.IdEscola
+                         LEFT JOIN TriagemEscola ON Escola.Id = TriagemEscola.IdEscola
+                WHERE TriagemEscola.IdEscola IS NULL;
+            `);
 
             for (const row of consulta.rows) {
                 data.push(await parseEscolaAutorizada({...row, id: row["escolaid"]}));
@@ -245,11 +311,11 @@ export namespace escolas {
             const {id, resposta} = req.body;
             console.log(id);
             console.log(resposta);
-            await db.pool.query("DELETE FROM TriagemEscola WHERE IdEscola = $1", [id]);
+            await db.positional("DELETE FROM TriagemEscola WHERE IdEscola = $1", [id]);
             if (resposta === "refuse") {
-                await db.pool.query("DELETE FROM ServidorEscola WHERE IdEscola = $1", [id]);
-                await db.pool.query("DELETE FROM EnderecoEscola WHERE IdEscola = $1", [id]);
-                await db.pool.query("DELETE FROM Escola WHERE Id = $1", [id]);
+                await db.positional("DELETE FROM ServidorEscola WHERE IdEscola = $1", [id]);
+                await db.positional("DELETE FROM EnderecoEscola WHERE IdEscola = $1", [id]);
+                await db.positional("DELETE FROM Escola WHERE Id = $1", [id]);
             }
         } catch (e) {
             console.log(e);
